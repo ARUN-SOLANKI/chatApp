e4596c88c96d2a1728174b4c20e3ee9cc46360c3
Arun solanki <arunsolanki301@gmail.com>
2022-06-21 16:24:03 +0530

    Profile

--- src/navigations/TopTabNavigators.js
+++ src/navigations/TopTabNavigators.js
@@ -9,7 +9,20 @@ const Tab = createMaterialTopTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: '500',
+          color: '#dfde33',
+          letterSpacing: 2,
+        },
+        // tabBarItemStyle: {width: 100},
+        tabBarStyle: {backgroundColor: '#577072'},
+        tabBarIndicatorStyle: {
+          color: '#fff',
+        },
+      }}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Posts" component={Posts} />
       <Tab.Screen name="Profile" component={Profile} />
--- src/screens/Posts.js
+++ src/screens/Posts.js
@@ -1,14 +1,31 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, TouchableOpacity, View, Text} from 'react-native';
 import React from 'react';
 
 const Posts = () => {
   return (
-    <View>
-      <Text>Reels</Text>
+    <View style={styles.PostContainer}>
+      <TouchableOpacity style={styles.postBtn}>
+        <Text style={styles.postBtnText}>Add Post</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Posts;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  PostContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  postBtn: {
+    backgroundColor: '#dfde33',
+    padding: 15,
+    borderRadius: 5,
+  },
+  postBtnText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+});
