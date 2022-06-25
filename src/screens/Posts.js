0da8a92164440cb3ad1ca62037763ae897bb2e20
Arun solanki <arunsolanki301@gmail.com>
2022-06-25 12:49:00 +0530

    post page

--- src/screens/Posts.js
+++ src/screens/Posts.js
@@ -5,48 +5,48 @@ import {
   Text,
   DevSettings,
   Image,
-  FlatList
-} from "react-native";
-import ImagePicker from "react-native-image-crop-picker";
-import storage from "@react-native-firebase/storage";
-import firestore from "@react-native-firebase/firestore";
-import React, { useState, useEffect } from "react";
-import { getItem } from "../utils/AsyncStorage";
-import _ from "underscore";
-import PostComponent from "../components/PostComponent";
-import { useFocusEffect } from "@react-navigation/native";
-const PostCollection = firestore().collection("Posts");
+  FlatList,
+} from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
+import storage from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore';
+import React, {useState, useEffect} from 'react';
+import {getItem} from '../utils/AsyncStorage';
+import _ from 'underscore';
+import PostComponent from '../components/PostComponent';
+import {useFocusEffect} from '@react-navigation/native';
+const PostCollection = firestore().collection('Posts');
 
 const Posts = () => {
   const [UserInfo, setUserInfo] = useState({
-    email: "",
-    uid: ""
+    email: '',
+    uid: '',
   });
   const [Data, setData] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
   const AddPost = async () => {
     try {
       const image = await ImagePicker.openPicker({
-        mediaType: "any"
+        mediaType: 'any',
       });
-      let fileNameArray = image.path.split("/");
+      let fileNameArray = image.path.split('/');
       let fileName = `${fileNameArray[fileNameArray.length - 1]}`;
       const reference = storage().ref(`${fileName}`);
       let task = await reference.putFile(image.path);
       const url = await storage().ref(fileName).getDownloadURL();
       PostCollection.doc(UserInfo.uid).set({
         email: UserInfo.email,
-        uid: UserInfo.uid
+        uid: UserInfo.uid,
       });
       const PostCollectionRef = PostCollection.doc(UserInfo.uid)
-        .collection("post")
+        .collection('post')
         .doc()
         .set({
           collectionId: UserInfo.uid,
           email: UserInfo.email,
           postName: fileName,
           task: task.metadata,
-          imageUrl: url
+          imageUrl: url,
         });
     } catch (error) {
       console.log(error);
@@ -55,19 +55,16 @@ const Posts = () => {
 
   useEffect(() => {
     const getcollection = async () => {
-      const res = await getItem("EMAIL");
-      const res1 = await getItem("UID");
-      setUserInfo({ email: res, uid: res1 });
+      const res = await getItem('EMAIL');
+      const res1 = await getItem('UID');
+      setUserInfo({email: res, uid: res1});
     };
     getcollection();
   }, []);
 
-  useEffect(
-    () => {
-      getdataftat();
-    },
-    [UserInfo.uid]
-  );
+  useEffect(() => {
+    getdataftat();
+  }, [UserInfo.uid]);
 
   const getdataftat = async () => {
     const newData = [];
@@ -75,11 +72,14 @@ const Posts = () => {
     const data = subscriber._docs.map(item => item.id);
     for (const post of data) {
       try {
-        const postRef = await PostCollection.doc(post).collection("post").get();
+        const postRef = await PostCollection.doc(post).collection('post').get();
         postRef._docs.forEach(item => {
           return newData.push(item._data);
         });
-        const sortedData = _.sortBy(newData, item => item.task.timeCreated);
+        const sortedData = _.sortBy(
+          newData,
+          item => item.task.timeCreated,
+        ).reverse();
         setData(sortedData);
       } catch (error) {
         console.log(error);
@@ -98,7 +98,7 @@ const Posts = () => {
     <View style={styles.PostContainer}>
       <FlatList
         data={Data}
-        renderItem={({ item }) => {
+        renderItem={({item}) => {
           return <PostComponent item={item} />;
         }}
         refreshing={refreshing}
@@ -112,6 +112,6 @@ export default Posts;
 
 const styles = StyleSheet.create({
   PostContainer: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
